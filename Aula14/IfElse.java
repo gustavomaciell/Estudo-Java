9ac1d1963ea82f6193934c291164282f09e18197
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-05-06 08:50:54 -0300

    Add Aula14

---
+++ Aula14/IfElse.java
@@ -0,0 +1,43 @@
+package Aula14;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        /*System.out.println("Digite sua idade:");
+        int idade = scan.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Maior de idade.");
+        } else {
+            System.out.println("Menor de idade.");
+        }*/
+
+        //EX 2:
+
+        /*
+         * Barato <= 10
+         * 10 < valor < 15 - pode pedir desconto
+         * 15 <= valor < 17 - pesquisar mais
+         * >= 17 - muito caro*/
+
+        System.out.println("Entre com o preço do item");
+        double valor = scan.nextDouble();
+
+        if (valor <= 10) {
+            System.out.println("Está barato, pode comprar.");
+        } else if (valor > 10 && valor < 15) {
+            System.out.println("Você pode pedir um desconto.");
+        } else if (valor >= 15 && valor <17) {
+            System.out.println("Pode pesquisar mais.");
+        } else { // valor >=17
+            System.out.println("Muito caro.");
+            
+        }
+
+        scan.close();
+    }
+}
