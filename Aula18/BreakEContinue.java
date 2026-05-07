e6dd3da434759a1645eeaa1f0b9bc330e1e13040
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-05-07 10:40:59 -0300

    Add Aula18

---
+++ Aula18/BreakEContinue.java
@@ -0,0 +1,31 @@
+package Aula18;
+
+import java.util.Scanner;
+
+public class BreakEContinue {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com um número: ");
+        int num = scan.nextInt();
+
+        System.out.println("Entre com um limite: ");
+        int limite = scan.nextInt();
+
+        for (int i = num; i <= limite; i++) {
+            if (i % 7 == 0){
+                System.out.println("O valor de i é: " + i);
+                break;
+            }
+        }
+        for (int i = num; i <= limite; i++) {
+            if (i % 7 == 0){
+                continue;
+            }
+            System.out.println("O valor de i é: " + i);
+        }
+
+        scan.close();
+    }
+}
