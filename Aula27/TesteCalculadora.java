6e5f94372bb0113f424f14155734091cd86590e4
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-05-28 01:58:07 -0300

    Add método static

--- Aula27/TesteCalculadora.java
+++ Aula27/TesteCalculadora.java
@@ -3,12 +3,9 @@ package Aula27;
 public class TesteCalculadora {
     public static void main(String[] args) {
 
-        MinhaCalculadora calculadora = new MinhaCalculadora();
-
-
-        System.out.println(calculadora.soma(1, 2));
-        System.out.println(calculadora.soma(1.5, 2.8));
-        System.out.println(calculadora.soma(1,5,9));
+        System.out.println(MinhaCalculadora.soma(1, 2));
+        System.out.println(MinhaCalculadora.soma(1.5, 2.8));
+        System.out.println(MinhaCalculadora.soma(1,5,9));
 
     }
 }
